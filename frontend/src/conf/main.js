7f8ad6a0d50ac1809f05c6cd63577a8c314b2a7c
Chaithanan <chaithanan.pk5199@gmail.com>
2025-03-06 18:53:07 +0700

    config api

--- frontend/src/conf/main.js
+++ frontend/src/conf/main.js
@@ -2,9 +2,9 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const conf = {
 	isProd,
-	url: isProd ? 'https://http://34.124.175.248' :"http://localhost:1337"  ,
-	apiUrlPrefix: isProd ? 'https://http://34.124.175.248/api' :"http://localhost:1337/api",
-    globalchat: isProd ? 'https://http://34.124.175.248' : "http://localhost"
+	url: isProd ? 'https://sda-lastgame-437542875768.asia-southeast1.run.app' :"http://localhost:1337"  ,
+	apiUrlPrefix: isProd ? 'https://sda-lastgame-437542875768.asia-southeast1.run.app/api' :"http://localhost:1337/api",
+    globalchat: isProd ? 'https://sda-lastgame-437542875768.asia-southeast1.run.app' : "http://localhost"
 };
 
 export default conf;
