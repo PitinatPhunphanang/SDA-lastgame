7931d7f2cdcc2993695e16411784b1aa0a8b8302
Chaithanan <chaithanan.pk5199@gmail.com>
2025-03-06 17:59:12 +0700

    ip

--- frontend/src/conf/main.js
+++ frontend/src/conf/main.js
@@ -2,9 +2,9 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const conf = {
 	isProd,
-	url: isProd ? 'https://http://35.240.177.163' :"http://localhost:1337"  ,
-	apiUrlPrefix: isProd ? 'https://http://35.240.177.163/api' :"http://localhost:1337/api",
-    globalchat: isProd ? 'https://http://35.240.177.163' : "http://localhost"
+	url: isProd ? 'https://http://34.124.175.248' :"http://localhost:1337"  ,
+	apiUrlPrefix: isProd ? 'https://http://34.124.175.248/api' :"http://localhost:1337/api",
+    globalchat: isProd ? 'https://http://34.124.175.248' : "http://localhost"
 };
 
 export default conf;
